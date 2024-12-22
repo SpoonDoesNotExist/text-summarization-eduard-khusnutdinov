8eea8637f3b0a8ee9c2557ece6e080dbd5e9cd92
Eduard <69144718+SpoonDoesNotExist@users.noreply.github.com>
2024-12-22 06:21:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit as st
 from transformers import pipeline
 import time
 
+
 @st.cache_resource
 def load_summarizer():
     return pipeline("summarization")
+
 
 summarizer = load_summarizer()
 
@@ -16,13 +18,12 @@ input_text = st.text_area("Input Text", height=200)
 if st.button("Generate Summary"):
     if input_text.strip():
         with st.spinner("Generating summary, please wait..."):
-            time.sleep(1) 
+            time.sleep(1)
 
-            summary = summarizer(input_text, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+            summary = summarizer(input_text, max_length=130, min_length=30, do_sample=False)[0]['summary_text']  # noqa
 
             st.success("Summary generated successfully!")
             st.write("### Summary")
             st.write(summary)
     else:
         st.error("Please enter some text to summarize.")
-
